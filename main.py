06bf5f364f816add88aada21b699a6183398d661
Jahanvi13 <97488922+Jahanvi13@users.noreply.github.com>
2022-05-04 22:39:15 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+#
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
+#
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# model. add(tf.keras.layers.Dense(128, activation='relu'))
+# model. add(tf.keras.layers.Dense(128, activation='relu'))
+# model. add(tf.keras.layers.Dense(10, activation='softmax'))
+#
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#
+# model.fit(x_train, y_train, epochs=7)
+#
+# model.save('handwritten.model')
+
+# ---------------------------------------------------------------------
+
+model = tf. keras.models.load_model('handwritten.model')
+
+pic_number = 1
+while os.path.isfile(f"digits/digit{pic_number}.png"):
+    try:
+        pic = cv2.imread(f"digits/digit{pic_number}.png")[:,:,0]
+        pic = np.invert(np.array([pic]))
+        prediction = model.predict(pic)
+        print(f"This digit is probably a {np.argmax(prediction)}")
+        plt.imshow(pic[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error!")
+    finally:
+        pic_number += 1
+
+
+
